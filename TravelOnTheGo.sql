7cd61f7dd6612eb3c1fce4b02a244fb6b0929817
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2021-11-13 14:03:16 +0530

    Updated column name and query
    Passanger_name is not Passenger_name
    Q6 now has a working query

--- TravelOnTheGo.sql
+++ TravelOnTheGo.sql
@@ -1,22 +1,24 @@
 create schema TravelOnTheGo;
 use TravelOnTheGo;
 
+-- Q1 - You are required to create two tables PASSENGER and PRICE with the following attributes and properties
 CREATE TABLE PASSENGER (
-    Passanger_name VARCHAR(100),
-    Categroy VARCHAR(50),
+    PASSENGER_name VARCHAR(100),
+    Category VARCHAR(50),
     Gender VARCHAR(1),
     Boarding_City VARCHAR(50),
     Destination_City VARCHAR(50),
     Distance INT,
     Bus_type VARCHAR(20)
 );
-
+-- 
 CREATE TABLE PRICE (
     Bus_type VARCHAR(20),
     Distance INT,
     Price INT
 );
 
+-- Q2 - Insert the following data in the tables
 insert into PRICE values('Sleeper', 350, 770);
 insert into PRICE values('Sleeper', 500, 1100);
 insert into PRICE values('Sleeper', 600, 1320);
@@ -31,7 +33,7 @@ insert into PRICE values('Sitting', 700, 868);
 insert into PRICE values('Sitting', 1000, 1240);
 insert into PRICE values('Sitting', 1200, 1488);
 insert into PRICE values('Sitting', 1500, 1860);
-
+-- 
 insert into PASSENGER values('Sejal', 'AC', 'F', 'Bengaluru', 'Chennai', 350, 'Sleeper');
 insert into PASSENGER values('Anmol', 'Non-AC', 'M', 'Mumbai', 'Hyderabad', 700, 'Sitting');
 insert into PASSENGER values('Pallavi', 'AC', 'F', 'Panaji', 'Bengaluru', 600, 'Sleeper');
@@ -42,7 +44,7 @@ insert into PASSENGER values('Hemant', 'Non-AC', 'M', 'Panaji', 'Mumbai', 700, '
 insert into PASSENGER values('Manish', 'Non-AC', 'M', 'Hyderabad', 'Bengaluru', 500, 'Sitting');
 insert into PASSENGER values('Piyush', 'AC', 'M', 'Pune', 'Nagpur', 700, 'Sitting');
 
--- Q3 - How many females and how many male passengers travelled for a minimum distance of 600 KM s?
+-- Q3 - How many females and how many male PASSENGERs travelled for a minimum distance of 600 KM s?
 SELECT 
     GENDER, COUNT(GENDER)
 FROM
@@ -59,33 +61,50 @@ FROM
 WHERE
     Bus_type = 'Sleeper';
     
--- Q5 - Select passenger names whose names start with character 'S'
+-- Q5 - Select PASSENGER names whose names start with character 'S'
 SELECT 
     *
 FROM
     PASSENGER
 WHERE
-    Passenger_name LIKE 'S%';
+    PASSENGER_name LIKE 'S%';
     
--- Q6 - Calculate price charged for each passenger displaying Passenger name, Boarding City, Destination City, Bus_Type, Price in the output
+-- Q6 - Calculate price charged for each PASSENGER displaying PASSENGER name, Boarding City, Destination City, Bus_Type, Price in the output
+SELECT 
+    Pass.PASSENGER_name,
+    Pass.Boarding_city,
+    Pass.Destination_city,
+    P.Bus_Type,
+    P.Price
+FROM
+    PASSENGER AS Pass,
+    PRICE AS P
+WHERE
+    Pass.Bus_type = P.Bus_type
+        AND Pass.Distance = P.Distance
+ORDER BY CASE
+    WHEN Pass.Category = 'AC' THEN P.Price
+END DESC , CASE
+    WHEN Pass.Category = 'Non-AC' THEN P.Price
+END ASC;
 
--- Q7 - What is the passenger name and his/her ticket price who travelled in Sitting bus for a distance of 1000 KMs
+-- Q7 - What is the PASSENGER name and his/her ticket price who travelled in Sitting bus for a distance of 1000 KMs
 
 -- Q8 - What will be the Sitting and Sleeper bus charge for Pallavi to travel from Bangalore to Panaji?
 
--- Q9 - List the distances from the "Passenger" table which are unique (non-repeated distances) in descending order.
+-- Q9 - List the distances from the "PASSENGER" table which are unique (non-repeated distances) in descending order.
 SELECT DISTINCT
     (Distance)
 FROM
     PASSENGER
 ORDER BY Distance DESC;
 
--- Q10 - Display the passenger name and percentage of distance travelled by that passenger from the total distance travelled by all passengers without using user variables
+-- Q10 - Display the PASSENGER name and percentage of distance travelled by that PASSENGER from the total distance travelled by all PASSENGERs without using user variables
 -- SELECT 
---     Passenger_name, Distance / Tot_Dist * 100
+--     PASSENGER_name, Distance / Tot_Dist * 100
 -- FROM
 --     PASSENGER
--- GROUP BY Passenger_name, Distance;
+-- GROUP BY PASSENGER_name, Distance;
 
 -- Q11 - Display the distance, price in three categories in table Price
 -- a) Expensive if the cost is more than 1000
